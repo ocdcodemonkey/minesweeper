ce84f0fb215ee88b4f48ba379c15919d193bf1af
Robert Piper <rwpiper@ocdcodemonkey.net>
2012-06-30 17:17:45 +0100

    Added basic interaction and styling.

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -5,13 +5,14 @@ minesweeper =
 {
 	width:		20,
 	height:		20, 
-	mines:		10,
+	mines:		100,
 
 	limit:		0,
 	grid:		null,
 
 	init: function()
 	{
+		this.$e = $('#minesweeper');
 		this.limit = this.width * this.height;
 		this.grid = [];
 
@@ -30,9 +31,43 @@ minesweeper =
 				}
 			}
 		}
-		return this;
 
+		this.$e.width((this.width * 20) + 'px');
+		this.$e.height((this.height * 20) + 'px');
+
+		var me = this;
+		this.$e.click(function(event)
+		{
+			var pos = me.$e.position();
+			var x = Math.floor((event.clientX - pos.left) / 20);
+			var y = Math.floor((event.clientY - pos.top) / 20);
+
+			me.renderSquare((y * me.width) + x);
+		});
+	},
+
+	renderSquare: function(index)
+	{
+		$block = $('<div class="square"></div>');
+
+		if (this.grid[index] > 8) // Mine
+		{
+			$block.addClass('mine');
+		}
+		else if (this.grid[index] > 0) // Adjacent
+		{
+			$block.text(this.grid[index]);
+		}
+
+		var y = Math.floor(index / this.width);
+		var x = index - (y * this.width);
+
+		$block.css('left', (x * 20) + 'px');
+		$block.css('top', (y * 20) + 'px');
+
+		this.$e.append($block);
 	}
+
 
 };
 minesweeper.init();
